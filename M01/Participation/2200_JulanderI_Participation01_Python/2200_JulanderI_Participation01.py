e5934b67e3de78bcb8d46c32acca91b8af15387b
Cypher545 <11073864@uvu.edu>
2026-09-01 11:49:01 -0600

    Init commit for Python portion of M01 Participation

---
+++ M01/Participation/2200_JulanderI_Participation01_Python/2200_JulanderI_Participation01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#Name: Ian Julander
+#Class: INFO 2200
+#Section: 001
+#Professor: Xing
+#Date: 09/04/2026
+#Participation or Assignment #: M01 Participation
+#By submitting this assignment, I declare that the source code contained in this assignment was written solely by me, unless specifically provided in the assignment. I attest that no part of this assignment, in whole or in part, was directly created by Generative AI, unless explicitly stated in the assignment instructions, nor obtained from a subscription service. I understand that copying any source code, in whole or in part, unless specifically provided in the assignment, constitutes cheating, and that I will receive a zero on this project if I am found in violation of this policy.
+
+def main():
+
+
+
+
+'''
+checks whether the current module is being run as the main module; if so, it calls the main() function. otherwise, it does not call the main() function; this lets other programs import this module without running the main() function
+'''
+if __name__ == "__main__": main()
