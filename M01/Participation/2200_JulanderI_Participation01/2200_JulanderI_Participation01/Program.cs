d25858e8467ecd0d52f7847a3d730d86f1fdcc65
Cypher545 <11073864@uvu.edu>
2026-09-03 12:29:02 -0600

    Created C# portion of participation and added dictionary for state capitals

---
+++ M01/Participation/2200_JulanderI_Participation01/2200_JulanderI_Participation01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2200_JulanderI_Participation01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Ian's State Capital Lookup App");
+            // display title text
+            Console.WriteLine();
+            // blank line
+
+            Console.Write("Please enter a state and I will return the capital (x to exit): ");
+            // prompt user for input
+
+            string userInput = Console.ReadLine().ToLower();
+            // assign console input to string varaible and convert to lowercase
+
+
+        }
+    }
+}
